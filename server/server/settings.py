ed8ba9cc303c1a39c2da4d307d6d1f7b98b4165a
devkya <kihaok@gmail.com>
2023-06-01 17:32:32 +0900

    djanog environ 적용

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,16 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+env_path = BASE_DIR / ".env"
+if env_path.exists():
+    with env_path.open("rt", encoding="utf8") as f:
+        env.read_env(f)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gxr$la^j45)m+56m!a&g7_d&d4ab89mhg+cs$h5_@@^-v7d$0)"
+SECRET_KEY = env.str(
+    "SERET_KEY",
+    default="django-insecure-gxr$la^j45)m+56m!a&g7_d&d4ab89mhg+cs$h5_@@^-v7d$0)",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -109,7 +118,7 @@ AUTH_USER_MODEL = "accounts.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "UTC"
 
@@ -122,12 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = env.str("STATIC_ROOT", default=BASE_DIR / "staticfiles")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = env.str("MEDIA_ROOT", default=BASE_DIR / "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+INTERNAL_IPS = env.list("INTERNAL_IPS", default=["127.0.0.1"])
