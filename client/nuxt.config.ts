75f2fe38d797624006e044d0f88461613cf79e81
devKya <kihaok@gmail.com>
2023-06-04 10:03:45 +0900

    Update

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/styles/main.scss"],
+  app: {
+    head: {
+      title: "Devkya 쇼핑몰",
+      script: [
+        {
+          src: "https://cdn.iamport.kr/v1/iamport.js",
+        },
+      ],
+    },
+  },
+});
---
+++ client/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+if (process.client) {
+  const IMP = window.IMP;
+  const code = "iamport";
+  IMP.init(code);
+  const merchant_uid = "merchant_" + new Date().getTime();
+  console.log(merchant_uid);
+}
+</script>
+<template>
+  <div>
+    <h2>결제하기</h2>
+  </div>
+</template>
